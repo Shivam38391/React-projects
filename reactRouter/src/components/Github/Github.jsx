f60a97453e36aaa513a5b1a751737581d91dd5ec
shivam sharma <shivamsharma38391@gmail.com>
2023-10-04 12:31:33 +0530

    how to user loader in router how to call apis

--- reactRouter/src/components/Github/Github.jsx
+++ reactRouter/src/components/Github/Github.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
+import { useLoaderData } from 'react-router-dom'
+
 function Github() {
+
+    const data = useLoaderData()
+
+
   return (
-    <div className='text-center m-4 bg-slate-600 text-yellow-100 p-4 text-3xl'>Github Followers: </div>
+    <>
+    <div className='text-center m-4 bg-slate-600 text-yellow-100 p-4 text-3xl'>Github Followers:  {data.followers }</div>
+    <img src={data.avatar_url} alt="" />
+    </>
   )
 }
 
 export default Github
+
+export const  githubInfoLoader = async () => {
+    const response = await fetch("https://api.github.com/users/shivam38391")
+    console.log("api fire");
+    return response.json()
+}
